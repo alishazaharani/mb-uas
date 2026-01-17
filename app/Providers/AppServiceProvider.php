a32c80953bbf4040eb26ba46bfc9c147b31a466b
Alisha Zaharani <alishazaharani@gmail.com>
2026-01-17 13:47:44 +0700

    Correct the error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\View; // <--- tambahin ini
+use App\Models\Category;           
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
 {
     if (app()->environment('production')) {
         URL::forceScheme('https');
+    }
+    {
+        // Share $categories ke semua view
+        View::share('categories', Category::all());
     }
 }
 }
